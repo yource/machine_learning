9498c3afcac1dffde4b6567c2857977a44c761cf
yource <1135242645@qq.com>
2023-03-21 20:53:30 +0800

    查询设备

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-import numpy as np
+# import numpy as np
 
-a = np.zeros((2,3,4))
-print(a)
-print(a.shape)
-print(a.shape[-1])
+# a = np.zeros((2,3,4))
+# print(a)
+# print(a.shape)
+# print(a.shape[-1])
+
+import os
+from tensorflow.python.client import device_lib
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "99"
+ 
+if __name__ == "__main__":
+    print(device_lib.list_local_devices())
