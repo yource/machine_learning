4c1c732abdf86319257af2382825c0f87a8ec2e1
yource <1135242645@qq.com>
2023-04-24 22:07:34 +0800

    temp1

--- temp1.py
+++ temp1.py
@@ -42,10 +42,112 @@ def fftDate():
 
 # 拆分数据
 column_indices = {name: i for i, name in enumerate(df.columns)}
-
 n = len(df)
 train_df = df[0:int(n*0.7)]
 val_df = df[int(n*0.7):int(n*0.9)]
 test_df = df[int(n*0.9):]
-
 num_features = df.shape[1]
+# 归一化
+train_mean = train_df.mean()
+train_std = train_df.std()
+train_df = (train_df - train_mean) / train_std
+val_df = (val_df - train_mean) / train_std
+test_df = (test_df - train_mean) / train_std
+# 查看归一化后的特征分布
+def showStd():
+    df_std = (df - train_mean) / train_std
+    df_std = df_std.melt(var_name='Column', value_name='Normalized')
+    plt.figure(figsize=(12, 6))
+    ax = sns.violinplot(x='Column', y='Normalized', data=df_std)
+    _ = ax.set_xticklabels(df.keys(), rotation=90)
+    plt.show()
+
+# 数据窗口化
+class WindowGenerator():
+    def __init__(self, input_width, label_width, shift, train_df=train_df, val_df=val_df, test_df=test_df, label_columns=None):
+        # Store the raw data.
+        self.train_df = train_df
+        self.val_df = val_df
+        self.test_df = test_df
+        # Work out the label column indices.
+        self.label_columns = label_columns
+        if label_columns is not None:
+            self.label_columns_indices = {
+                name: i for i,name in enumerate(label_columns)}
+        self.column_indices = {name: i for i,name in enumerate(train_df.columns)}
+        # Work out the window parameters.
+        self.input_width = input_width
+        self.label_width = label_width
+        self.shift = shift
+        self.total_window_size = input_width + shift
+        self.input_slice = slice(0, input_width)
+        self.input_indices = np.arange(self.total_window_size)[self.input_slice]
+        self.label_start = self.total_window_size - self.label_width
+        self.labels_slice = slice(self.label_start, None)
+        self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
+
+    def __repr__(self):
+        return '\n'.join([
+            f'Total window size: {self.total_window_size}',
+            f'Input indices: {self.input_indices}',
+            f'Label indices: {self.label_indices}',
+            f'Label column name(s): {self.label_columns}'])
+    
+    def make_dataset(self, data):
+        data = np.array(data, dtype=np.float32)
+        ds = tf.keras.utils.timeseries_dataset_from_array(
+            data=data,
+            targets=None,
+            sequence_length=self.total_window_size,
+            sequence_stride=1,
+            shuffle=True,
+            batch_size=32,)
+        ds = ds.map(self.split_window)
+        return ds
+    
+    def split_window(self, features):
+        inputs = features[:, self.input_slice, :]
+        labels = features[:, self.labels_slice, :]
+        if self.label_columns is not None:
+            labels = tf.stack([labels[:, :, self.column_indices[name]] for name in self.label_columns], axis=-1)
+        # Slicing doesn't preserve static shape information, so set the shapes
+        # manually. This way the `tf.data.Datasets` are easier to inspect.
+        inputs.set_shape([None, self.input_width, None])
+        labels.set_shape([None, self.label_width, None])
+        return inputs, labels
+
+    @property
+    def train(self):
+        return self.make_dataset(self.train_df)
+
+    @property
+    def val(self):
+        return self.make_dataset(self.val_df)
+
+    @property
+    def test(self):
+        return self.make_dataset(self.test_df)
+
+    @property
+    def example(self):
+        """Get and cache an example batch of `inputs, labels` for plotting."""
+        result = getattr(self, '_example', None)
+        if result is None:
+            # No example batch was found, so get one from the `.train` dataset
+            result = next(iter(self.train))
+            # And cache it for next time
+            self._example = result
+        return result
+
+window = WindowGenerator(input_width=24, label_width=24, shift=1,label_columns=['T (degC)'])
+model = tf.keras.models.Sequential([
+    tf.keras.layers.LSTM(32, return_sequences=True),
+    tf.keras.layers.Dense(units=1)
+])
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',patience=2,mode='min')
+model.compile(loss=tf.keras.losses.MeanSquaredError(),
+            optimizer=tf.keras.optimizers.Adam(),
+            metrics=[tf.keras.metrics.MeanAbsoluteError()])
+history = model.fit(window.train, epochs=20,
+                    validation_data=window.val,
+                    callbacks=[early_stopping])
