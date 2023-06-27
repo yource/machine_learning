3221148bebb33b56e79778b8a25338d14e717440
yource <1135242645@qq.com>
2023-06-27 14:40:34 +0800

    处理数据

--- plan/step01.py
+++ plan/step01.py
@@ -67,12 +67,13 @@ def getData(code, min):
     file = "data/main/"+code+"_"+str(min)+"min.csv"
     df = pd.read_csv(file)
     cols = df.columns
-    df = df[[cols[2],cols[3],cols[4],cols[5],cols[6],cols[7],cols[8]]]
+    df = df[[cols[2],cols[3],cols[4],cols[5]]]
     df = df[1:]
     # print("##### 检查数据错误 最大值最小值")
     # print(df.head)
     # print(df.describe().transpose())
     
+    df = np.array(df, dtype=np.float32)
     n = len(df)
     # print("##### 数据归一化 减去平均值、除以标准差")
     # data_train = df[0: int(0.75 * n)]
@@ -80,16 +81,33 @@ def getData(code, min):
     # train_std = data_train.std(axis=0)
     # df = (df-train_mean)/train_std
 
-    print("##### 生成 样本-目标")
-    samples, targets = util.generator(df,24,1,6,0)
-
     print("##### 拆分数据 训练集、验证集、测试集")
-    data_train = df[0: int(0.7 * n)]
-    data_val = df[int(0.7 * n):int(0.9 * n)]
-    data_test = df[int(0.9 * n):]
+    data_train = df[0: int(0.7 * n)]  #约1911天的数据
+    data_val = df[int(0.7 * n):int(0.88 * n)]
+    data_test = df[int(0.88 * n):]
+
+    print("##### 生成 样本-目标")
+    lookback = 6
+    delay = 2
+    step = 1
+    offset = 2
+    train_x, train_y = util.generator(data_train,lookback=lookback,delay=delay,step=step,offset=offset)
+    val_x, val_y = util.generator(data_val,lookback=lookback,delay=delay,step=step,offset=offset)
+    test_x, test_y = util.generator(data_test,lookback=lookback,delay=delay,step=step,offset=offset)
+    # len1 = len(np.where(train_y<0)[0])
+    # len2 = len(np.where(train_y>0)[0])
+    # print(str(len1)+"+"+str(len2)+"="+str(len1+len2))
+    # print(str(len1+len2)+"/"+str(len(train_x))+"="+str((len1+len2)/len(train_x)))
+    # print(1911/(len1+len2))
+    print(train_x.shape)
+    print(train_y.shape)
+    print(val_x.shape)
+    print(val_y.shape)
+    print(test_x.shape)
+    print(test_y.shape)
+
     
     
 
 
-getData(varieties[0]["code"],5)
-
+getData(varieties[0]["code"],20)
--- plan/util.py
+++ plan/util.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 '''
-根据过去的lookback期数据 预测delay期之后的数据
+根据过去的lookback期数据 预测之后的第delay期的数据
 隔step取一个有效值
 隔offset个数据生成一个窗口 0代表各window不交叉
 '''
@@ -18,6 +18,15 @@ def generator(data, lookback, delay, step=1, offset=0):
     for i in range(0, window_count):
         window = data[offset*i:offset*i+window_size]
         window_samples = window[0:lookback]
+        window_sample_value = window[lookback-1][3]
+        window_target = window[lookback+delay-1]
+        window_target_high = (window_target[0]+window_target[1]+window_target[3])/3
+        window_target_low = (window_target[0]+window_target[2]+window_target[3])/3
+        window_label = 0
+        if(window_target_low>window_sample_value and window_target_high>window_sample_value*1.01):
+            window_label = 1
+        elif(window_target_low<window_sample_value*0.99 and window_target_high<window_sample_value):
+            window_label = -1
         window_mean = window_samples.mean(axis=0)
         window_std = window_samples.std(axis=0)
         for ii in range(0, 4):
@@ -25,5 +34,6 @@ def generator(data, lookback, delay, step=1, offset=0):
                 window_std[ii] = 1
         window = (window-window_mean)/window_std
         samples[i] = window[0:lookback:step]
-        targets[i] = window[lookback+delay-1][0]
+        # targets[i] = window[lookback+delay-1][0]
+        targets[i] = window_label
     return samples, targets
